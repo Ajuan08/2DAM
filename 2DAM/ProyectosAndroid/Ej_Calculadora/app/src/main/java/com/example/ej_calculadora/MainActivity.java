25ad4ae82a63e49aea4fcda8eb45e369e750f046
Ajuan08 <ariofer0809@g.educaand.es>
2023-10-16 09:10:06 +0200

    Calculadora_Funcional

--- 2DAM/ProyectosAndroid/Ej_Calculadora/app/src/main/java/com/example/ej_calculadora/MainActivity.java
+++ 2DAM/ProyectosAndroid/Ej_Calculadora/app/src/main/java/com/example/ej_calculadora/MainActivity.java
@@ -1,80 +1,72 @@
 package com.example.ej_calculadora;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView tv;
-    private String numeroActual = "";
-    private String operador = "";
-    private double resultado = 0;
+    TextView pantalla;
+    String operacion = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = (TextView) findViewById(R.id.pantalla);
+        pantalla = findViewById(R.id.pantalla);
     }
 
-    public void enClickBoton(View vista) {
-        Button boton = (Button) vista;
-        String textoBoton = boton.getText().toString();
+    public void onClickNumero(View v) {
+        Button btn = (Button) v;
+        operacion += btn.getText().toString();
+        pantalla.setText(operacion);
+    }
 
-        switch (textoBoton) {
-            case "=":
-                calcular();
-                break;
-            case "C":
-                limpiar();
-                break;
-            default:
-                actualizarPantalla(textoBoton);
-                break;
+    public void onClickOperador(View v) {
+        if (!operacion.equals("")) {
+            Button btn = (Button) v;
+            operacion += btn.getText().toString();
+            pantalla.setText(operacion);
         }
     }
 
-    private void actualizarPantalla(String textoBoton) {
-        numeroActual += textoBoton;
-        tv.setText(numeroActual);
-    }
+    public void onClickIgual(View v) {
+        if (!operacion.equals("")) {
+            String[] terminos = operacion.split("[+\\-X/]");
+            if (terminos.length == 2) {
+                double num1 = Double.parseDouble(terminos[0]);
+                double num2 = Double.parseDouble(terminos[1]);
+                char operador = operacion.charAt(terminos[0].length());
 
-    private void calcular() {
-        double segundoNumero = Double.parseDouble(numeroActual);
+                double resultado;
+                switch (operador) {
+                    case '+':
+                        resultado = num1 + num2;
+                        break;
+                    case '-':
+                        resultado = num1 - num2;
+                        break;
+                    case 'X':
+                        resultado = num1 * num2;
+                        break;
+                    case '/':
+                        resultado = num1 / num2;
+                        break;
+                    default:
+                        resultado = 0; //
+                        break;
+                }
 
-        switch (operador) {
-            case "+":
-                resultado += segundoNumero;
-                break;
-            case "-":
-                resultado -= segundoNumero;
-                break;
-            case "*":
-                resultado *= segundoNumero;
-                break;
-            case "/":
-                resultado /= segundoNumero;
-                break;
-            default:
-                resultado = segundoNumero;
-                break;
+                operacion = String.valueOf(resultado);
+                pantalla.setText(operacion);
+            }
         }
-
-        tv.setText(String.valueOf(resultado));
-        numeroActual = "";
-        operador = "";
     }
 
-    private void limpiar() {
-        numeroActual = "";
-        operador = "";
-        resultado = 0;
-        tv.setText("");
+    public void onClickReset(View v) {
+        operacion = "";
+        pantalla.setText("0");
     }
 }
