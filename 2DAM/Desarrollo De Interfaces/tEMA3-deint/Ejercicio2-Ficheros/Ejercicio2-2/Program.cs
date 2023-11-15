c2604928eff7aef4ab611bfaeddd6a0cb2853e55
Ajuan08 <ariofer0809@g.educaand.es>
2023-11-15 10:57:09 +0100

    Terminar_Ejercicio2-DEINT

---
+++ 2DAM/Desarrollo De Interfaces/tEMA3-deint/Ejercicio2-Ficheros/Ejercicio2-2/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.IO;
+
+Console.WriteLine("Introduce el nombre de un directorio");
+string nombreDirectorio = Console.ReadLine();
+
+
---
+++ 2DAM/Desarrollo De Interfaces/tEMA3-deint/Ejercicio2-Ficheros/Ejercicio2-Ficheros/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+Console.WriteLine("Introduce el nombre de un directorio");
+string nombreDirectorio = Console.ReadLine();
+
+    if (Directory.Exists(nombreDirectorio))
+    {
+        string[] archivos = Directory.GetFiles(nombreDirectorio);
+        if (archivos.Length > 0)
+        {
+            string ficheroReciente = calcularFicheroReciente(archivos);
+            Console.WriteLine($"El fichero mas reciente es: {ficheroReciente}");
+            
+        }else
+        {
+            Console.WriteLine($"El directorio {nombreDirectorio} existe, pero no tiene archivos");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El directorio no existe");
+    }
+
+string calcularFicheroReciente(string[] archivos)
+{
+    DateTime fecha = DateTime.MinValue;
+    var archivoADevolver = "";
+
+    foreach (var item in archivos)
+    {
+        DateTime fech = File.GetLastWriteTime(item);
+
+        if (fech > fecha)
+        {
+            fecha = fech;
+            archivoADevolver = item;
+        }
+    }
+    return archivoADevolver;
+}
+
+
