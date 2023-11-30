8d68f2fbd9e5fb04c163d11992d046bc69a85e81
Ajuan08 <ariofer0809@g.educaand.es>
2023-12-01 00:11:38 +0100

    01/12/2023

--- 2DAM/Desarrollo De Interfaces/tEMA3-deint/EjercicioJardineria/EjercicioJardineria/obj/Debug/net8.0-windows/EjercicioJardineria.AssemblyInfo.cs
+++ 2DAM/Desarrollo De Interfaces/tEMA3-deint/EjercicioJardineria/EjercicioJardineria/obj/Debug/net8.0-windows/EjercicioJardineria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioJardineria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba23ea4fc0a4b52e2888fbe20a166c84d68b0a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioJardineria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjercicioJardineria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
